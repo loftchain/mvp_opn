4ed28af526fd6bb51b9e23441db6499588093cb5
nkt <nkt.millers@gmail.com>
2018-10-24 08:28:40 +0000

    from server commit

--- source/white-list.php
+++ source/white-list.php
@@ -34,16 +34,12 @@ function connect_db() {
   return $mysqli;
 }
 
-
 $mysqli = connect_db();
 
 $ip = get_ip();
 
-$sql = "INSERT INTO `mvp_white_list` (`email`, `ip`) 
-        VALUES ('".$_POST['email']."','$ip')";
+$sql = "INSERT INTO `mvp_white_list` (`email`, `ip`) VALUES ('".$_POST['email']."','$ip')";
 
-send_hook($_POST['email'], $ip);
-echo $sql;
 if (!$mysqli->query($sql)) {
     echo("error recovery bd");
     exit;
@@ -51,52 +47,24 @@ if (!$mysqli->query($sql)) {
 
 $mysqli->close();
 
-
-
-
-
+send_hook($_POST['email'], $ip);
 
 function send_hook($email, $ip){
-    $webhookUrl = "https://discordapp.com/api/webhooks/503568677363384330/6UpK1lOoGXvaUpemuK5Y-j6xaJXNvVKHkrDl2BoQyJcr144CAYch16mZLWd1ZT7gaOTj";
-
-    $email_from_name = 'opnplatform';
-    $email_from_email = 'notifications.opnplatform@gmail.com';
-    $list_id = 15226169;
 // Создаём POST-запрос
     $request = [
-        'email' => $email,
-        'ip' => $ip,
-
+        'content' => '**email: **' . $email. "\n" . 
+                      '**ip: **' . $ip. "\n" .
+                      '--------------------------',
     ];
 
 // Устанавливаем соединение
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $ch = curl_init("https://discordapp.com/api/webhooks/503568677363384330/6UpK1lOoGXvaUpemuK5Y-j6xaJXNvVKHkrDl2BoQyJcr144CAYch16mZLWd1ZT7gaOTj");
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
 
     $result = curl_exec($ch);
 
     if ($result) {
-        // Раскодируем ответ API-сервера
-        $jsonObj = json_decode($result);
-
-        if (null === $jsonObj) {
-            // Ошибка в полученном ответе
-            echo 'Invalid JSON';
-
-        } elseif (!empty($jsonObj->error)) {
-            // Ошибка отправки сообщения
-            echo sprintf('An error occured %s (code: %s)', $jsonObj->error, $jsonObj->code);
-        } else {
-            // Сообщение успешно отправлено
-            echo 'Email message is sent. Message id ' . $jsonObj->result->email_id;
-
-        }
-    } else {
-        // Ошибка соединения с API-сервером
-        echo 'API access error';
+      echo $result;
     }
 }
